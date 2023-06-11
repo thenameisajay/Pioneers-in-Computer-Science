9a39675aacc5e1aee23517ff59663c69f89a03bc
apm30 <m.ajaypradeep@icloud.com>
2023-06-11 20:30:24 +0100

    Added favicons and css files to the front page of the website.

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+
 const app = express();
 
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 
 app.get('/', (req, res) => {
