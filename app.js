7ee2b091050ba0c2a45792542f1a57b721f41d03
thenameisajay <m.ajaypradeep@icloud.com>
2023-06-14 14:22:18 +0100

    Connected search function with the backend and data is being displayed on thhe terminal.

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+
+app.route("/search").post((req, res) => {
+  const name = req.body.search;
+  console.log(name);
+  console.log("searching for " + name);
+  res.redirect("/");
+});
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
