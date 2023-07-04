4976215e674dd000ff9a7d77d32dd6f861abc7ef
thenameisajay <m.ajaypradeep@icloud.com>
2023-07-04 21:12:48 +0100

    by name search page has been updated with pagination.

--- routes/by_name_search.js
+++ routes/by_name_search.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const Pioneer = require('../models/pioneer');  // assuming the models directory is at the root level
+const Pioneer = require('../models/pioneer');
 const _ = require('lodash');
-
 
 router.get("/:alphabet", (req, res) => {
     console.log("Search Results Page has been requested");
@@ -11,18 +10,27 @@ router.get("/:alphabet", (req, res) => {
     console.log("The search term is: " + searchTerm);
     const searchRegex = _.toLower(searchTerm.trim().replace(/[-\s]+/g, "[\\s-]*"));
 
+    let page = Number(req.query.page) || 1;
+    let pioneersPerPage = 10;
+    let skip = (page - 1) * pioneersPerPage;
+
     // Find all the pioneers whose names match with the first letter of the alphabet.
-    Pioneer.find({ Name: { $regex: new RegExp("^" + searchRegex, "i") } }).exec().then((pioneers) => {
-        console.log("Found " + pioneers.length + " pioneers");
-        res.render("by_name_search", { pioneers: pioneers, alphabet: searchRegex });
-    }).catch((err) => {
+    Pioneer.find({ Name: { $regex: new RegExp("^" + searchRegex, "i") } })
+    .skip(skip)
+    .limit(pioneersPerPage)
+    .exec()
+    .then((pioneers) => {
+        // count the total number of pioneers
+        Pioneer.countDocuments({ Name: { $regex: new RegExp("^" + searchRegex, "i") } })
+        .then((totalPioneers) => {
+            console.log("Found " + pioneers.length + " pioneers");
+            res.render("by_name_search", { pioneers: pioneers, alphabet: searchRegex, totalPioneers: totalPioneers, pioneersPerPage: pioneersPerPage });
+        });
+    })
+    .catch((err) => {
         console.log("Error retrieving pioneers");
         res.redirect("/error");
-
-    }
-    );
+    });
 });
-
-
 
 module.exports = router;
