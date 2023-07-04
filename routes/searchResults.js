4d8e289e47bdf58df95999e39d21e89a101dcb60
thenameisajay <m.ajaypradeep@icloud.com>
2023-07-04 21:21:30 +0100

    now pagified for the searches available in the website.

--- routes/searchResults.js
+++ routes/searchResults.js
@@ -1,29 +1,36 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const Pioneer = require('../models/pioneer');  // assuming the models directory is at the root level
+const Pioneer = require('../models/pioneer');
 const _ = require('lodash');
-
 
 router.get("/:searchTerm", (req, res) => {
     console.log("Search Results Page has been requested");
     const searchTerm = req.params.searchTerm;  // Retrieve the name parameter from the URL path
     console.log("The search term is: " + searchTerm);
     const searchRegex = _.toLower(searchTerm.trim().replace(/[-\s]+/g, "[\\s-]*"));
-    
+
+    let page = Number(req.query.page) || 1; // Fetch page number from query parameters, default to 1
+    let pioneersPerPage = 10; // You can adjust this value according to your needs
+    let skip = (page - 1) * pioneersPerPage;
+
     // Find all the pioneers whose names match the search term
-    Pioneer.find({ Name: { $regex: new RegExp(".*" + searchRegex + ".*", "i") } }).exec().then((pioneers) => {
-        console.log("Found " + pioneers.length + " pioneers");
-        
-        res.render("searchResults", { pioneers: pioneers, searchTerm: searchTerm });
-    }).catch((err) => {
+    Pioneer.find({ Name: { $regex: new RegExp(".*" + searchRegex + ".*", "i") } })
+    .skip(skip)
+    .limit(pioneersPerPage)
+    .exec()
+    .then((pioneers) => {
+        // Count total pioneers matching the search term for pagination
+        Pioneer.countDocuments({ Name: { $regex: new RegExp(".*" + searchRegex + ".*", "i") } })
+        .then((totalPioneers) => {
+            console.log("Found " + pioneers.length + " pioneers");
+            res.render("searchResults", { pioneers: pioneers, searchTerm: searchTerm, totalPioneers: totalPioneers, pioneersPerPage: pioneersPerPage });
+        });
+    })
+    .catch((err) => {
         console.log("Error retrieving pioneers");
-        res.render("searchResults", { pioneers: [], searchTerm: searchTerm });
-
-    }
-    );
+        res.render("error");
+    });
 });
-
-
 
 module.exports = router;
