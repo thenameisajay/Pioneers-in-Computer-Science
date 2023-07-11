b6e87135780bd4cfe4425602e6da0b93fc46c5c0
apm30 <m.ajaypradeep@icloud.com>
2023-07-11 09:01:39 +0100

    change in the comments and file name bug.

--- models/Chat.js
+++ models/Chat.js
@@ -1,13 +1,18 @@
 const mongoose = require('mongoose');
 
+
+//Defining the schema for the chat data
 const chatSchema = new mongoose.Schema({
   message: String,
   answer: String,
   date: { type: Date, default: Date.now }
 });
 
+
+
 // Creating the database model
 const Chat = mongoose.model("Chat", chatSchema);
+
 
 
 module.exports = Chat;
