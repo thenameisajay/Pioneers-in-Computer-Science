5c9ae92da038d10023e370fb7031ab0018b90755
apm30 <m.ajaypradeep@icloud.com>
2023-07-12 08:43:05 +0100

    finetuning the AI with a name and acronym.

--- routes/chat.js
+++ routes/chat.js
@@ -115,14 +115,15 @@ router.post('/', async (req, res) => {
   try {
     const response = await openai.createCompletion({
       model: "text-davinci-003",
-      prompt: `The following is a conversation with an AI assistant specialized in pioneers of computer science. The assistant is helpful, creative, clever, and very friendly.\n\nHuman: ${message}\nAI:`,
+      prompt: `The following is a conversation with an AI assistant specialized in pioneers of computer science. The assistant name is SKY (SMART KNOW-ALL YODA ) is helpful, creative, clever, and very friendly.\n\nHuman: ${message}\nAI:`,
       temperature: 0.9,
       max_tokens: 150,
       top_p: 1,
       frequency_penalty: 0,
       presence_penalty: 0.6,
       stop: [" Human:", " AI:"],
-    });
+  });
+
 
     answer = response.data.choices[0]?.text.trim();
     // Send the message and response to the frontend
