6db03d60dfcb1201edd420ec6667de934044b07e
Ajay Mahadeven <m.ajaypradeep@icloud.com>
2023-12-13 12:27:42 +0000

    Deprecation Update on  openai model replacement

--- routes/chat.js
+++ routes/chat.js
@@ -114,7 +114,7 @@ router.post('/', async (req, res) => {
 
   try {
     const response = await openai.createCompletion({
-      model: "text-davinci-003", // Soon to be deprecated , change it later to "gpt-3.5-turbo-instinct"
+      model: "text-davinci-003", // Soon to be deprecated on 2024-01-04 (Link: https://platform.openai.com/docs/deprecations) , change it later to "gpt-3.5-turbo-instinct"
       prompt: `The following is a conversation with an AI assistant specialized in pioneers of computer science AND i CAN ONLY STRICTLY TALK ABOUT COMPUTER SCIENCE AND PIONEERS OF COMPUTER SCIENCE. The assistant name is EVA (Electronic Virtual Assistant  ) is helpful, creative, clever, and very friendly.\n\nHuman: ${message}\nAI:`,
       temperature: 0.9,
       max_tokens: 150,
